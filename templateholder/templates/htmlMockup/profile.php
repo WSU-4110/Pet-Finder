5748f3890e942cd8aebf26c3c77fbc9890e5ce47
Ali98Wayne <71354440+Ali98Wayne@users.noreply.github.com>
2020-11-24 20:05:01 -0500

    Frontend updates, Closes #3, #18, #19

--- templateholder/templates/htmlMockup/profile.php
+++ templateholder/templates/htmlMockup/profile.php
@@ -3,13 +3,13 @@
     function display_table() {        
 		$result = get_profile_info();
 			foreach ($result as $row) {
-				echo "<tr>" . "<td>" . "<h2><b>Username: </b></h2>" . "<h3>" . $row[0] . "</h3>" . "</td>" . "</tr>";
-				echo "<tr>" . "<td>" . "<h2><b>First Name: </b></h2>" . "<h3>" . $row[1] . "</h3>" . "</td>" . "</tr>";
-				echo "<tr>" . "<td>" . "<h2><b>Last Name: </b></h2>" . "<h3>" . $row[2] . "</h3>" . "</td>" . "</tr>";
-				echo "<tr>" . "<td>" . "<h2><b>Address: </b></h2>" . "<h3>" . $row[3] . "</h3>" . "</td>" . "</tr>";
-				echo "<tr>" . "<td>" . "<h2><b>State: </b></h2>" . "<h3>" . $row[4] . "</h3>" . "</td>" . "</tr>";
-				echo "<tr>" . "<td>" . "<h2><b>Phone: </b></h2>" . "<h3>" . $row[5] . "</h3>" . "</td>" . "</tr>";
-				echo "<tr>" . "<td>" . "<h2><b>About Me: </b></h2>" . "<h3>" . $row[6] . "</h3>" . "<br><br><br><br><br>" . "</td>" . "</tr>";
+				echo "<tr>" . "<td>" . "<h2><b>Username: </b></h2>" . '<div style="margin-left: auto; margin-right: auto;">' . "<h3>" .$row[0] . "</h3>" . '</div>' . "</td>" . "</tr>";
+				echo "<tr>" . "<td>" . "<h2><b>First Name: </b></h2>" . '<div style="margin-left: auto; margin-right: auto;">' . "<h3>" .$row[1] . "</h3>" . '</div>' . "</td>" . "</tr>";
+				echo "<tr>" . "<td>" . "<h2><b>Last Name: </b></h2>" . '<div style="margin-left: auto; margin-right: auto;">' . "<h3>" .$row[2] . "</h3>" . '</div>' . "</td>" . "</tr>";
+				echo "<tr>" . "<td>" . "<h2><b>Address: </b></h2>" . '<div style="margin-left: auto; margin-right: auto;">' . "<h3>" .$row[3] . "</h3>" . '</div>' . "</td>" . "</tr>";
+				echo "<tr>" . "<td>" . "<h2><b>State: </b></h2>" . '<div style="margin-left: auto; margin-right: auto;">' . "<h3>" .$row[4] . "</h3>" . '</div>' . "</td>" . "</tr>";
+				echo "<tr>" . "<td>" . "<h2><b>Phone: </b></h2>" . '<div style="margin-left: auto; margin-right: auto;">' . "<h3>" .$row[5] . "</h3>" . '</div>' . "</td>" . "</tr>";
+				echo "<tr>" . "<td>" . "<h2><b>About Me: </b></h2>" . '<h3 style="word-wrap: break-word; background-color: white; width: 35%; height:20%; margin-left: auto; margin-right: auto;">' .$row[6] . "</h3>" . "<br><br><br>" . "</td>" . "</tr>";
         }
     }
 ?>
@@ -18,21 +18,21 @@
 <head>						<!-- Initialize head content & external links -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pet&ndash;Finder</title>
+    <title>My Profile</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="styles.css">
 </head>
 <body style="background: #9FE5FF;"> <!-- Body Content -->
     <div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
         <a class="pure-menu-heading" href="index.html">Pet Finder</a>
-        <ul class="pure-menu-list"> <!-- Links to other pages -->
-            <li class="pure-menu-item"><a href="index.html" class="pure-menu-link"><b>Home</b></a></li>
-            <li class="pure-menu-item"><a href="questionnaire.html" class="pure-menu-link"><b>Questionnaire</b></a></li>
+		<ul class="pure-menu-list"> <!-- Links to other pages -->
+			<li class="pure-menu-item"><a href="index.html" class="pure-menu-link"><b>Home</b></a></li>
+			<li class="pure-menu-item"><a href="questionnaire.html" class="pure-menu-link"><b>Questionnaire</b></a></li>
 			<li class="pure-menu-item"><a href="browse.html" class="pure-menu-link"><b>Browse</b></a></li>
-            <li class="pure-menu-item"><a href="signup.html" class="pure-menu-link"><b>Sign Up</b></a></li>
+			<li class="pure-menu-item"><a href="signup.html" class="pure-menu-link"><b>Sign Up</b></a></li>
 			<li class="pure-menu-item"><a href="signin.html" class="pure-menu-link"><b>Sign In</b></a></li>
 			<li class="pure-menu-item"><a href="profile.php" class="pure-menu-link"><i class="fa fa-user-circle"></i><b> My Profile</b></a></li>
-        </ul>
+		</ul>
     </div>
 									<!-- Profile Pic -->
     <div><a><img src="images/flower.jpg" style="position: absolute; margin-top: 5%; margin-left: 5%;"></a></div>
