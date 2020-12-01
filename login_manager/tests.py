eaf4527914a2fac7bb670a2ebc4e7e1a75439355
alexkishekedu <go9709@wayne.edu>
2020-12-01 16:22:57 -0500

    unit test updates

--- login_manager/tests.py
+++ login_manager/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from login_manager.views import *
 
 # Create your tests here.
+def test_login_page(request):
+	assert login_page(request) == 'login page loaded'
+
+def test_register_page(request):
+	assert register_page(request) == 'register page loaded'
+
+def test_login_user(request):
+	assert login_user(request) == True
+
+def test_register_user(request):
+	assert register_user(request) == "success"
+
+def test_forgot_user(request):
+	assert forgot_user(request) == "success"	
--- startpage/tests.py
+++ startpage/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from startPage.views import *
 
 # Create your tests here.
+def test_home(request):
+	assert home(request) == 'home page loaded'
+
+def test_petview(request):
+	assert petview(request) == 'success'
