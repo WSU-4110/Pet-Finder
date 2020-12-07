58f1eba01ccacce14479027d47e2c92377372165
drfrink <pradeepbhatnerd@gmail.com>
2020-12-07 17:49:43 -0500

    shows results of questionaire without filtering, closes #8,FR8

--- questionnaireManager/views.py
+++ questionnaireManager/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import logout
 from django.shortcuts import render
-
+from petManager.models import pets
 
 # Create your views here.
 def quiz(request, error=None, info=None, success=None, warning=None):
@@ -16,6 +16,9 @@ def quiz(request, error=None, info=None, success=None, warning=None):
     if request.method == 'POST':
         data = request.POST
         print(data)
+        petResults = pets.objects.all()
+        context = {'title': 'Pet Results'}
+        return render(request, 'app/questionnaireManager/results_page.html', {'context': context, 'petresults': petResults})
     return render(request, 'app/questionnaireManager/questionnaire_page.html', {'context': context})
 
 def results(request, error=None, info=None, success=None, warning=None):
